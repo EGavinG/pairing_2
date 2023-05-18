dce006454c3b68147a9685846a25c9f7f1f64970
Gavin Garcia <edwardgavingarcia@yahoo.com>
2023-05-17 19:09:28 -0500

    Initial Commit

---
+++ plants.js
@@ -0,0 +1,8 @@
+var gets_sunshine = false
+var ounces_of_water = 16
+if (gets_sunshine === true && ounces_of_water >= 10){
+    console.log("This plant is thriving!");
+}
+else {
+    console.log("This plant will die.");
+}
